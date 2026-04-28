4c9eb1130bb02c27f0213e280ca1c5c1503b168e
Draiphis <val08350@gmail.com>
2026-04-28 09:41:43 +0200

    first commit

---
+++ src/test/java/com/mns/cda/my_recipe_api/MyRecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mns.cda.my_recipe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
